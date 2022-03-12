8fa82a2b876949d13e8199a2f8647439097e3e69
konovalovily <konovalovily0987@gmail.com>
2022-03-12 13:22:11 +0400

    item for rv

--- app/src/main/java/ru/ssau/reviewzor/presenter/screen/ListPlacesFragment.kt
+++ app/src/main/java/ru/ssau/reviewzor/presenter/screen/ListPlacesFragment.kt
@@ -1,6 +1,8 @@
 package ru.ssau.reviewzor.presenter.screen
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import ru.ssau.reviewzor.databinding.FragmentListPlacesBinding
 import ru.ssau.reviewzor.presenter.base.BaseFragment
 
@@ -9,5 +11,9 @@ class ListPlacesFragment : BaseFragment<FragmentListPlacesBinding>() {
     override fun initBinding(inflater: LayoutInflater): FragmentListPlacesBinding =
         FragmentListPlacesBinding.inflate(inflater)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        TODO("Задать ресайкл адаптер")
+    }
 
 }
